8e5026578393072015977f345532c05abe066f82
IgnaDev <arcortoon@gmail.com>
2022-03-19 21:05:57 -0300

    Arranque con WebComponents - Modalida simple, estructura de carpetas

---
+++ Temas/WebComponents/1-EjemploDeEstructuraDeCarpetas/index.js
@@ -0,0 +1,7 @@
+/*En la carpeta assets se guardan los ficheros estaticos (Imagenes, videos, fotografias).
+Mientras que en components se utilizara para guardar todos los archivos .js que dentro 
+tengan un WebComponent*/
+
+//Esta manera de acomodar las cosas esta mas centrada en el uso de los modulos, es simple y directa.
+
+//Para el tema del servidor local tengo instalado web-dev-server, para iniciarlo usar "npm run start"
