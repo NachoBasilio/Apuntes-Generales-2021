6a649e9e929f37c91621059605095ffac3e43789
IgnaDev <arcortoon@gmail.com>
2022-03-21 19:02:26 -0300

    ¡Mi primer componente! - #WebComponentsWeek

---
+++ Temas/WebComponents/3-ApuntesDeDirectoManzDev/2-convencionesYEjemplosDeCustomsElement.js
@@ -0,0 +1,5 @@
+//La convención para nombrar un webComponent es poner un guión bajo entre las palabras que forman el nombre del mismo.
+//Ahora si ¿Como creamos un webComponent? Bueno, para comenzar vamos a crear un fichero aparte para el componente, normalmente se crea una carpeta llamada components y eso voy a hacer
+
+//Ahora vamos a importar el componente que hemos creado
+import "./components/HeaderPortafolio.js"
---
+++ Temas/WebComponents/3-ApuntesDeDirectoManzDev/components/HeaderPortafolio.js
@@ -0,0 +1,16 @@
+//Digamos que quiero que este componente lo que haga es contener una imagen y un texto. La etiqueta va a ser <header-portafolio></header-portafolio> y mientras no hagamos nada va a actuar como un div.
+
+ 
+//Los componentes se crean de manera nativa por medio de clases, esta clase cre vamos a crear tiene que extender de HTMLElement.
+class HeaderPortafolio extends HTMLElement{
+    constructor(){ //Siempre comenzamos con el constructor y su super ya que extiende de HTMLElement
+        super()
+        //this.textContent = "Portafolio" Con this vamos a hacer referencia la instancia de la clase, cosa curiosa porque en este caso nos vamos a referir a nuestro componente que ahora se encuentra en el DOM. Así podemos agregar cosas a nuestro componente y que se vea en la página.
+        this.innerHTML = `<div class="header">
+        <img src="https://pbs.twimg.com/profile_images/1469812812511948802/42JLNnH1_400x400.png" alt="">
+        <h1>Igancio Basilio / @ignadev</h1>
+        `//Esto es lo que realmente me interesa, el innerHTML es una propiedad que nos permite agregar código html dentro de nuestro componente.
+    }
+}
+//Esto es un componente, inutil pero lo es. Ahora solo falta asociarlo a una etiqueta.
+customElements.define("header-portafolio", HeaderPortafolio)
