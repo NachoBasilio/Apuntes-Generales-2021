353f076de8da2c3cf8ffe69fb1a4006f70409dd7
IgnaDev <arcortoon@gmail.com>
2022-03-21 20:05:56 -0300

    Primera parte connectedCallback - #WebComponentsWeek / Arranque con las notas de coderHouse

--- Temas/WebComponents/3-ApuntesDeDirectoManzDev/3-connectedCallback.js
+++ Temas/WebComponents/3-ApuntesDeDirectoManzDev/3-connectedCallback.js
@@ -2,4 +2,8 @@
 //Ahora si ¿Como creamos un webComponent? Bueno, para comenzar vamos a crear un fichero aparte para el componente, normalmente se crea una carpeta llamada components y eso voy a hacer
 
 //Ahora vamos a importar el componente que hemos creado
-import "./components/2-HeaderPortafolio.js"
+import "./components/3-HeaderPortafolio..js"
+
+//Ahora vamos a tomar el componente crear un elemento y meterlo en el body
+const headerPortafolio = document.createElement("header-portafolio")
+// document.body.appendChild(headerPortafolio)
