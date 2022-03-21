095f1f9e8beb5edfbf1ee25ba6427fe8aa8bb777
IgnaDev <arcortoon@gmail.com>
2022-03-21 15:04:49 -0300

    Vamos a ver el super directo de manz - #WebComponentsWeek

---
+++ Temas/WebComponents/3-ApuntesDeDirectoManzDev/1-queEsUnWebComponent.js
@@ -0,0 +1,11 @@
+//Con WebComponents vamos a descomponer todo en pequeños elementos asi haciendo más fácil el trabajar en nuestras paginas web.
+//"Web Components es una serie de características, en la que la principal es crearnos nuestra propia etiqueta"
+//Esto es un estándar, con el cual utilizando JS, HTML y CSS podemos crear nuestros propios componentes, sin necesidad de alguna cuestión por fuera de estos.
+ 
+//Los componentes tienen dos características muy importantes: encapsulación e interoperabilidad.
+//Encapsulamiento: es decir, que no se puede acceder a los atributos o métodos de un componente desde fuera de este.
+//Interoperabilidad: es decir, que nuestro componente se puede comunicar con otros componentes, incluyendo componentes de otros frameworks.
+
+//Las caracteristicas que tienen los WebComponents son: "custom elements", "templates", "shadow DOM", "modules" y "css Scopes". Todo esto lo vamos a explicar mas adelante.
+
+
