b795131eb7d18c8a0b384e1e7abf601cdd6dc74e
IgnaDev <arcortoon@gmail.com>
2022-03-22 10:13:16 -0300

    disconnectedCallback - #WebComponentWeek

--- Temas/WebComponents/3-ApuntesDeDirectoManzDev/4-disconnectedCallback.js
+++ Temas/WebComponents/3-ApuntesDeDirectoManzDev/4-disconnectedCallback.js
@@ -2,8 +2,18 @@
 //Ahora si ¿Como creamos un webComponent? Bueno, para comenzar vamos a crear un fichero aparte para el componente, normalmente se crea una carpeta llamada components y eso voy a hacer
 
 //Ahora vamos a importar el componente que hemos creado
-import "./components/3-HeaderPortafolio.js"
+import "./components/4-HeaderPortafolio.js"
 
 //Ahora vamos a tomar el componente crear un elemento y meterlo en el body
 const headerPortafolio = document.createElement("header-portafolio")
 // document.body.appendChild(headerPortafolio)
+
+const destroy = () =>{
+
+    const element = document.querySelector("header-portafolio")
+    element.remove()
+} 
+
+setTimeout(() => {
+    destroy()
+}, 5000)
