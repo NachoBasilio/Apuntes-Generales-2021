8a821ddda01d6f763b4ce968d28427d752a68ff8
IgnaDev <arcortoon@gmail.com>
2022-03-22 10:28:18 -0300

    Atributos y su manejo - #WebComponentsWeek

---
+++ Temas/WebComponents/3-ApuntesDeDirectoManzDev/components/5-HeaderPortafolio.js
@@ -0,0 +1,53 @@
+//Digamos que quiero que este componente lo que haga es contener una imagen y un texto. La etiqueta va a ser <header-portafolio></header-portafolio> y mientras no hagamos nada va a actuar como un div.
+
+ 
+//Los componentes se crean de manera nativa por medio de clases, esta clase cre vamos a crear tiene que extender de HTMLElement.
+class HeaderPortafolio extends HTMLElement{
+    constructor(){ //Siempre comenzamos con el constructor y su super ya que extiende de HTMLElement
+        super()
+        //this.textContent = "Portafolio" Con this vamos a hacer referencia la instancia de la clase, cosa curiosa porque en este caso nos vamos a referir a nuestro componente que ahora se encuentra en el DOM. Así podemos agregar cosas a nuestro componente y que se vea en la página.
+        // this.innerHTML = `<div class="header">
+        // <img src="https://pbs.twimg.com/profile_images/1469812812511948802/42JLNnH1_400x400.png" alt="">
+        // <h1>Igancio Basilio / @ignadev</h1>
+        // `//Esto es lo que realmente me interesa, el innerHTML es una propiedad que nos permite agregar código html dentro de nuestro componente.
+    }
+    connectedCallback(){//Este metodo es un especie de constructor para webcomponent, es decir, cuando se cargue el componente en el DOM, este metodo se va a ejecutar.
+        // this.innerHTML = `<div class="header">
+        // <img src="https://pbs.twimg.com/profile_images/1469812812511948802/42JLNnH1_400x400.png" alt="">
+        // <h1>Igancio Basilio / @ignadev</h1>
+        // `
+        this.nombre = this.getAttribute("nombre") ?? "Nacho"//Asi podemos tomar los atributos que se han puesto en el componente.
+        this.render()//Render es una función que se va a ejecutar en el momento que se cargue el componente en el DOM para pintar el componente. Otros framkworks no necesitan de este metodo.
+    }
+
+    // render(){//Vamos a usar la api del DOM de html para pintar el componente. Las dos son buenas maneras de hacerlo.
+    //     const h1 = document.createElement("h1")
+    //     h1.textContent = this.nombre
+
+    //     const img = document.createElement("img")
+    //     img.src = "https://pbs.twimg.com/profile_images/1469812812511948802/42JLNnH1_400x400.png"
+
+    //     const header = document.createElement("header")
+    //     header.appendChild(img)
+    //     header.appendChild(h1)
+
+    //     this.appendChild(header)
+    // }
+
+    disconnectedCallback(){//Este metodo es un especie de destructor para webcomponent, es decir, cuando se elimine el componente del DOM, este metodo se va a ejecutar.
+        console.log("Se ha eliminado el componente")
+
+    }
+    
+    
+
+    render(){
+        this.innerHTML = `<div class="header">
+        <img src="https://pbs.twimg.com/profile_images/1469812812511948802/42JLNnH1_400x400.png" alt="">
+        <h1>${this.nombre}/ @ignadev</h1>
+        `
+    }
+}
+
+//Esto es un componente, inutil pero lo es. Ahora solo falta asociarlo a una etiqueta.
+customElements.define("header-portafolio", HeaderPortafolio)
