37938e44267ee099975db3f199d697aa67707102
IgnaDev <arcortoon@gmail.com>
2022-03-29 17:28:28 -0300

    Voy a hacer un componente que va a ser una card por asi decirlo

--- Temas/WebComponents/4-creandoComponentesNativos/webcoponentsejemplos/src/components/PersonajeDiver.js
+++ Temas/WebComponents/4-creandoComponentesNativos/webcoponentsejemplos/src/components/PersonajeDiver.js
@@ -1,26 +1,34 @@
+const DEFAULT_FOTO_PERFIL = "https://images.vexels.com/media/users/3/160249/isolated/lists/14fab0425b54f10ea6f8e23265d4c620-silueta-de-pluma.png";
+
 class PersonajeDiver extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-}
+  }
 
   static get styles() {
     return `
    :host {;
-
+      display: block;
+      background-color: #f5f5f5;
    }
+    img {
+      width: 100px;
+    }
  `;
-}
+  }
 
   connectedCallback() {
+    this.fotoPerfil = this.getAttribute("fotoPerfil") ?? DEFAULT_FOTO_PERFIL;
     this.render();
-}
+  }
 
   render() {
     this.shadowRoot.innerHTML = `
   <style>${PersonajeDiver.styles}</style>
   <div>
-    <h1>Personaje Diver</h1>
+    <img src="${this.fotoPerfil}" alt="">
+    <h2>Personaje Diver</h2>
   </div>`;
   }
 }
