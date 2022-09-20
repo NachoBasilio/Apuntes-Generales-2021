1b0ae34a48cfc32404ecfa94a2f39d900124b671
IgnaDev <arcortoon@gmail.com>
2022-09-20 17:11:39 -0300

    Primer descanso

---
+++ Temas/Asincronismo/callback/index.js
@@ -0,0 +1,17 @@
+//Un callback es una funcion que se pasa como argumento a otra funcion, luego dentro de esa funcion se ejecutara.
+
+function suma(num1, num2) {
+    return num1 + num2;
+}
+
+function calc(num1, num2, callback){
+    return callback(num1, num2);
+}
+
+console.log(calc(1,2,suma))
+
+function saludo(){
+    console.log("Hola")
+}
+
+setTimeout(saludo, 5000)
