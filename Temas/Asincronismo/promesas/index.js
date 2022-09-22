7ab37a097c5d81991b19d7ca94ab23f300fffdf1
IgnaDev <arcortoon@gmail.com>
2022-09-22 13:01:39 -0300

    termine con promesas 2.0

--- Temas/Asincronismo/promesas/index.js
+++ Temas/Asincronismo/promesas/index.js
@@ -2,3 +2,22 @@ const promise = new Promise(function(resolve, reject){//Resolve y reject pueden 
     resolve("hey!")
 })
 
+const nombre = "Julsian"
+
+const JulianControler = new Promise(function(resolve, reject){
+    if(nombre == "Julian"){
+        resolve("Si sos")
+    }else{
+        reject("No sos >:C")
+    }
+})
+
+JulianControler
+.then(function(data){
+    console.log(data)
+})
+.catch(function(err){
+    console.log(err)
+}).finally(function(){
+    console.log("Todo finalizo de manera exitosa")
+})
